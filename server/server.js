d1d0851bd98462ea936104520213548680c83f7d
sajjan <sajjan@example.com>
2025-11-30 18:29:57 +0530

    some change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const fs = require('fs')
 dotenv.config();
 
 const app = express();
@@ -14,6 +14,15 @@ app.use(cors({
   credentials: true
 }));
 
+
+const options = {
+  key: fs.readFileSync(__dirname + '/ssl/key.pem'),
+  cert: fs.readFileSync(__dirname + '/ssl/cert.pem')
+};
+
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
